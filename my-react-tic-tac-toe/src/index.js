137e6c632f092a88a1886064334ed6048adb102e
Phillip Thain <pthain@purdue.edu>
2019-09-24 16:39:17 -0400

    Added \'O\'s

--- my-react-tic-tac-toe/src/index.js
+++ my-react-tic-tac-toe/src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-
+/*
 class Square extends React.Component {
   render() {
     return (
@@ -16,26 +16,43 @@ class Square extends React.Component {
     );
   }
 }
+*/
+
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  )
+}
+
 
 class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = 'X'
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   renderSquare(i) {
     return (
       <Square
         value={this.state.squares[i]}
-        onClick{() => this.handleClick(i)}
+        onClick = {() => this.handleClick(i)}
       />
     )
   }
